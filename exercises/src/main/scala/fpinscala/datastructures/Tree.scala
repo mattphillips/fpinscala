d6c3f47fbef0fe669425887b5438e822aa3398b7
Matt Phillips <matt@mattphillips.io>
2018-11-26 07:39:36 +0000

    Add tree data structure exercises

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,7 +7,36 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
+  def size[A](t: Tree[A]): Int = t match {
+    case Branch(left, right) => 1 + size(left) + size(right)
+    case Leaf(_) => 1
+  }
 
+  def maximum(t: Tree[Int]): Int = t match {
+    case Branch(l, r) => maximum(l) max maximum(r)
+    case Leaf(n) => n
+  }
 
+  def depth[A](t: Tree[A]): Int = t match {
+    case Branch(l, r) => 1 + depth(l) max depth(r)
+    case Leaf(n) => 0
+  }
 
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(a) => Leaf(f(a))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(a) => f(a)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+
+  def sizeViaFold[A](t: Tree[A]): Int = fold(t)(_ => 1)(1 + _ + _)
+
+  def maximumViaFold(t: Tree[Int]): Int = fold(t)(a => a)(_ max _)
+
+  def depth[A](t: Tree[A]): Int = fold(t)(_ => 0)(1 + _ max _)
+
+  def mapViaFold[A, B](t: Tree[A])(f: A => B): Tree[B] = fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_, _))
 }
